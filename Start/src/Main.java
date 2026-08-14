d525a277c69f7d293f5083a8fa2491cb2e6c7778
woodskathy2006-cell <woodskathy2006@gmail.com>
2026-08-13 19:18:33 -0600

    Added activities

--- Start/src/Main.java
+++ Start/src/Main.java
@@ -1,10 +1,5 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-
-import Controllers.*;
-import Viewers.*;
-
 void main() {
-    Console.writeln("EA Sports: It's in the game");
-    MainMenu.Run();
+
 }
---
+++ Start/src/Models/GeneralActivites.java
@@ -0,0 +1,15 @@
+package Models;
+
+public enum GeneralActivites {
+    Soccor,
+    baskectball,
+    baseball,
+    volleyball,
+    tennis, 
+    swimming, 
+    Rock_Climbing, 
+    long_Distance,
+    Short_Distance,
+    
+
+}
